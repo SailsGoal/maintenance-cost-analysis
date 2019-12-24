03f0dfe72b1649e9de77f1bc394135e7b5860e04
Ryan Meador <ryan@sailsgoal.com>
2019-12-23 19:31:44 -0500

    load data, adjust for inflation, calculate some simple values

---
+++ index.js
@@ -0,0 +1,75 @@
+import csv from 'csvtojson'
+import _ from 'underscore'
+
+const CURRENT_YEAR = 2019;
+
+async function loadData() {
+  return csv({
+    checkType: true,
+  })
+  .fromFile('./data.csv');
+}
+
+// consumer price index
+class InflationAdjuster {
+  constructor(map) {
+    this.map = map;
+  }
+
+  static async load() {
+    const map = new Map();
+
+    return csv({
+        delimiter: "auto",
+        checkType: true,
+        ignoreEmpty: true
+      })
+      .fromFile('./cu.data.2.Summaries')
+      .subscribe(async row => {
+        // filter annual data for all urban consumers
+        // except 2019, which is still in progress, so we pick the last month in the data set
+        // TODO if it's 2020 or later when you're reading this, you can remove this hack
+        if (row.series_id === 'CUUR0000SA0' && (row.period === 'M13' || (row.period === 'M11' && row.year === 2019))) {
+          map.set(row.year, row.value);
+        }
+      })
+      .then(() => {
+        return new InflationAdjuster(map);
+      });
+  }
+
+  adjust(value, fromYear, toYear) {
+    return value * this.map.get(toYear) / this.map.get(fromYear);
+  }
+}
+
+// calculate the percentage of the new-price the vessel still held at its most recent purchase
+function calcDepreciation(boats) {
+  return _.chain(boats)
+    .map(boat => _.pick(boat, 'ageAtPurchase', 'depreciatedValue', 'purchasePrice'))
+    .sortBy('ageAtPurchase')
+    .value();
+}
+
+async function main() {
+  const boats = await loadData();
+  const inflationAdjuster = await InflationAdjuster.load();
+
+  // massage data
+  boats.forEach(boat => {
+    boat.adjustedPurchasePrice = inflationAdjuster.adjust(boat.purchasePrice, boat.purchaseYear, CURRENT_YEAR);
+    if (boat.newPrice) {
+      boat.adjustedNewPrice = inflationAdjuster.adjust(boat.newPrice, boat.builtYear, CURRENT_YEAR);
+      boat.depreciatedValue = boat.adjustedPurchasePrice / boat.adjustedNewPrice;
+    }
+    boat.ageNow = CURRENT_YEAR - boat.builtYear;
+    boat.ageAtPurchase = boat.purchaseYear - boat.builtYear;
+  });
+
+  const boatsWithNewPrice = _.filter(boats, boat => Boolean(boat.newPrice));
+
+  console.log(boatsWithNewPrice);
+  console.log(calcDepreciation(boatsWithNewPrice));
+}
+
+main();
